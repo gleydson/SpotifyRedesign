3c0214a0e35800f6ee83b79a78855881f1027ce4
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-03-12 02:31:00 -0300

    Improve styles

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -1,18 +1,35 @@
+import { Platform, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('screen');
+
 export default {
   title: 'dark',
 
   colors: {
     primary: '#25A84E',
-    backgroundPrimary: '#191918',
-    backgroundSecondary: '#101010',
+    backgroundPrimary: '#101010',
+    backgroundSecondary: '#191918',
     text: '#EEE',
-    disable: '#555',
+    regular: '#999',
+    dark: '#555',
+    darker: '#222',
   },
   fonts: {
-    fontFamily: 'IBM Plex Mono',
+    fontFamilyBold: Platform.select({
+      ios: 'IBM Plex Mono',
+      android: 'IBM Plex Mono Bold',
+    }),
+    fontFamilyItalic: 'IBM Plex Mono Italic',
+    fontFamilyLight: 'IBM Plex Mono Light',
+    fontFamilyMedium: 'IBM Plex Mono Medium',
     big: '20px',
     medium: '16px',
     small: '12px',
+    smaller: '8px',
     letterSpacing: '2px',
+  },
+  metrics: {
+    widthScreen: width,
+    heightScreen: height,
   },
 };
