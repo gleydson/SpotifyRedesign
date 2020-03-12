a9bf6149c4a72e4bbc2ab562f0e2333b2ee8427f
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-03-12 02:29:11 -0300

    Add playlist screen

--- src/screens/playlist/index.tsx
+++ src/screens/playlist/index.tsx
@@ -1,7 +1,118 @@
+import { useRoute, useNavigation } from '@react-navigation/native';
 import React from 'react';
 
-import { Container } from './styled';
+import Button from '@components/button';
+import Header from '@components/header';
+
+import {
+  SafeAreaView,
+  IMusicList,
+  Container,
+  ContainerHeaderPlaylist,
+  PlaylistImage,
+  PlaylistCreatedBy,
+  ContainerButtons,
+  ContainerMusicList,
+  TouchableMusicItem,
+  MusicItem,
+  Thumbnail,
+  ContainerText,
+  Title,
+  Description,
+  Icon,
+} from './styled';
+
+const blinding = require('@assets/images/cards/playlist/blindign_lights.png');
+const candide = require('@assets/images/cards/playlist/candide_crush.png');
+const dance_monkey = require('@assets/images/cards/playlist/dance_monkey.png');
+const elthon = require('@assets/images/cards/playlist/elthon.png');
+const oui = require('@assets/images/cards/playlist/oui_ou_non.png');
+const shotgun = require('@assets/images/cards/playlist/shotgun.png');
+const stressed = require('@assets/images/cards/playlist/stressed_out.png');
+const vibes = require('@assets/images/cards/playlist/vibes.png');
+
+const playlistData: IMusicList[] = [
+  {
+    thumbnail: blinding,
+    title: 'Blinding Lights',
+    artist: 'The Weeknd',
+  },
+  {
+    thumbnail: candide,
+    title: 'Candide Crush',
+    artist: 'Thérapie Taxi',
+  },
+  {
+    thumbnail: dance_monkey,
+    title: 'Dance Monkey',
+    artist: 'Tones and I',
+  },
+  {
+    thumbnail: shotgun,
+    title: 'Shotgun',
+    artist: 'George Erza',
+  },
+  {
+    thumbnail: elthon,
+    title: 'Success',
+    artist: 'Elton John',
+  },
+  {
+    thumbnail: oui,
+    title: 'Oui ou Non',
+    artist: 'Angèle',
+  },
+  {
+    thumbnail: stressed,
+    title: 'Stressed Out',
+    artist: 'twenty one pilots',
+  },
+  {
+    thumbnail: vibes,
+    title: 'Vibes',
+    artist: 'The Vibes',
+  },
+];
 
 export default function playlist() {
-  return <Container />;
+  const { params } = useRoute();
+  const navigation = useNavigation();
+
+  function goToPlayer() {
+    navigation.navigate('Player');
+  }
+
+  return (
+    <SafeAreaView>
+      <Container>
+        <Header
+          title={params.headerTitle}
+          backButtonEnabled
+          rightIcon='more-horizontal'
+        />
+        <ContainerHeaderPlaylist>
+          <PlaylistImage source={params.playlistImage} />
+          <PlaylistCreatedBy>{params.createdBy}</PlaylistCreatedBy>
+        </ContainerHeaderPlaylist>
+        <ContainerButtons>
+          <Button icon='play' buttonDescription='Play' />
+          <Button icon='shuffle' buttonDescription='Shuffle' />
+        </ContainerButtons>
+        <ContainerMusicList>
+          {playlistData.map(item => (
+            <TouchableMusicItem key={item.artist} onPress={goToPlayer}>
+              <MusicItem>
+                <Thumbnail source={item.thumbnail} />
+                <ContainerText>
+                  <Title>{item.title}</Title>
+                  <Description>{item.artist}</Description>
+                </ContainerText>
+                <Icon name='more-horizontal' />
+              </MusicItem>
+            </TouchableMusicItem>
+          ))}
+        </ContainerMusicList>
+      </Container>
+    </SafeAreaView>
+  );
 }
--- src/screens/playlist/styled.ts
+++ src/screens/playlist/styled.ts
@@ -1,3 +1,88 @@
+import { ImageSourcePropType, StyleSheet } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${props => props.theme.colors.backgroundSecondary};
+`;
+
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
+  flex: 1;
+`;
+
+export const ContainerHeaderPlaylist = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: 20px 0;
+`;
+
+export const PlaylistImage = styled.Image`
+  width: 200px;
+  height: 200px;
+  border-radius: 11px;
+`;
+
+export const PlaylistCreatedBy = styled.Text`
+  font-family: ${props => props.theme.fonts.fontFamilyMedium};
+  font-size: ${props => props.theme.fonts.small};
+  color: ${props => props.theme.colors.regular};
+  padding-top: 5px;
+`;
+
+export const ContainerButtons = styled.View`
+  flex-direction: row;
+  padding: 20px;
+  justify-content: space-around;
+`;
+
+export interface IMusicList {
+  thumbnail: ImageSourcePropType;
+  title: string;
+  artist: string;
+}
+
+export const ContainerMusicList = styled.View`
+  padding: 20px 30px;
+`;
+
+export const TouchableMusicItem = styled.TouchableOpacity``;
+
+export const MusicItem = styled.View`
+  flex-direction: row;
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  border-style: solid;
+  border-top-color: ${props => props.theme.colors.darker};
+  padding: 10px 0;
+`;
+
+export const Thumbnail = styled.Image`
+  border-radius: 11px;
+`;
+
+export const ContainerText = styled.View`
+  flex: 1;
+  padding-left: 15px;
+`;
+
+export const Title = styled.Text`
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+  color: ${props => props.theme.colors.text};
+  font-family: ${props => props.theme.fonts.fontFamilyBold};
+  font-size: ${props => props.theme.fonts.small};
+`;
+
+export const Description = styled.Text`
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+  color: ${props => props.theme.colors.regular};
+  font-family: ${props => props.theme.fonts.fontFamilyMedium};
+  font-size: ${props => props.theme.fonts.smaller};
+`;
+
+export const Icon = styled(FeatherIcon).attrs(props => ({
+  size: 15,
+  color: props.theme.colors.dark,
+}))``;
