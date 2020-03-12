e5de02ce4551929b7d47a57ce97c5f2747f3db0a
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-03-12 02:28:56 -0300

    Add player screen

--- src/screens/player/index.tsx
+++ src/screens/player/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
 
-import { Container } from './styled';
+import Header from '@components/header';
+import Progress from '@components/progress';
+
+import {
+  SafeAreaView,
+  Container,
+  MusicImage,
+  Title,
+  Artist,
+  Icon,
+  IconImage,
+  Touchable,
+  ContainerMusicInfo,
+  ContainerMainIcons,
+  ContainerSecondaryIcons,
+} from './styled';
+
+const image = require('@assets/images/cards/player/blinding.png');
+const pause = require('@assets/images/cards/player/pause.png');
+const skipBack = require('@assets/images/cards/player/skip-back.png');
+const skipForward = require('@assets/images/cards/player/skip-forward.png');
 
 export default function player() {
-  return <Container />;
+  return (
+    <SafeAreaView>
+      <Container>
+        <Header title='Playing' rightIcon='more-horizontal' />
+        <ContainerMusicInfo>
+          <MusicImage source={image} />
+          <Title>Blinding Lights</Title>
+          <Artist>The Weeknd</Artist>
+        </ContainerMusicInfo>
+        <Progress totalTime={335000} currentTime={211030} />
+        <ContainerMainIcons>
+          <Touchable>
+            <IconImage source={skipBack} />
+          </Touchable>
+          <Touchable>
+            <IconImage source={pause} />
+          </Touchable>
+          <Touchable>
+            <IconImage source={skipForward} />
+          </Touchable>
+        </ContainerMainIcons>
+        <ContainerSecondaryIcons>
+          <Touchable>
+            <Icon name='cast' />
+          </Touchable>
+          <Touchable>
+            <Icon name='heart' />
+          </Touchable>
+          <Touchable>
+            <Icon name='list' />
+          </Touchable>
+        </ContainerSecondaryIcons>
+      </Container>
+    </SafeAreaView>
+  );
 }
--- src/screens/player/styled.ts
+++ src/screens/player/styled.ts
@@ -1,3 +1,61 @@
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${props => props.theme.colors.backgroundSecondary};
+`;
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const MusicImage = styled.Image`
+  border-radius: 11px;
+  width: ${props => props.theme.metrics.widthScreen * 0.85}px;
+  height: ${props => props.theme.metrics.widthScreen * 0.85}px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${props => props.theme.fonts.fontFamilyBold};
+  font-size: ${props => props.theme.fonts.big};
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+  color: ${props => props.theme.colors.text};
+  padding: 20px 0 8px 0;
+`;
+
+export const Artist = styled.Text`
+  font-family: ${props => props.theme.fonts.fontFamilyBold};
+  font-size: ${props => props.theme.fonts.small};
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+  color: ${props => props.theme.colors.primary};
+`;
+
+export const Icon = styled(FeatherIcon).attrs(props => ({
+  size: 30,
+  color: props.theme.colors.primary,
+}))``;
+
+export const IconImage = styled.Image``;
+
+export const Touchable = styled.TouchableOpacity``;
+
+export const ContainerMusicInfo = styled.View`
+  align-self: center;
+  padding: 20px 0;
+`;
+
+export const ContainerMainIcons = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+`;
+
+export const ContainerSecondaryIcons = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  padding: 20px;
+`;
