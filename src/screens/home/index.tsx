30bcc8f4dba3c9da79027af2f5fc7189ed59867b
Gleydson Rodrigues <gleydsonsr@gmail.com>
2020-03-12 02:29:38 -0300

    Fix home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,41 +1,139 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
+import { ImageSourcePropType } from 'react-native';
 
 import Carousel from '@components/carousel';
 import Header from '@components/header';
 
-import { SafeAreaView, Container, ContainerCarousel } from './styled';
+import {
+  SafeAreaView,
+  Container,
+  Title,
+  ContainerCarousel,
+  TouchableCard,
+  Session,
+  SessionScroll,
+  CardItem,
+  Card,
+  Label,
+  Description,
+} from './styled';
 
-const elton = require('@assets/images/cards/main/elton_john.png');
-const lalaland = require('@assets/images/cards/main/lalaland.png');
-const queen = require('@assets/images/cards/main/queen.png');
+const fell = require('@assets/images/cards/by_mood/fell.png');
+const fitness = require('@assets/images/cards/by_mood/fitness.png');
+const focus = require('@assets/images/cards/by_mood/focus.png');
+const motivation = require('@assets/images/cards/by_mood/motivation.png');
+const actual_hits = require('@assets/images/cards/dont_miss_it_today/actual_hits.png');
+const good_vibes = require('@assets/images/cards/dont_miss_it_today/good_vibes.png');
+const keep_the_move = require('@assets/images/cards/dont_miss_it_today/keep_the_move.png');
 const search = require('@assets/images/search/search.png');
 
 export default function home() {
-  const data = [
-    {
-      thumbnail: elton,
-      label: 'Elton John',
-    },
-    {
-      thumbnail: queen,
-      label: 'Legendary',
-    },
-    {
-      thumbnail: lalaland,
-      label: 'LaLa',
-    },
-  ];
+  const navigation = useNavigation();
+
+  function goToPlaylistScreen(
+    headerTitle: string,
+    playlistImage: ImageSourcePropType,
+    createdBy: string
+  ) {
+    navigation.navigate('Playlist', {
+      headerTitle,
+      playlistImage,
+      createdBy,
+    });
+  }
+
   return (
     <SafeAreaView>
       <Container>
-        <Header
-          title="Library"
-          rightIcon={search}
-          rightIconOnPress={() => { }}
-        />
+        <Header title='Library' rightIcon={search} />
         <ContainerCarousel>
-          <Carousel title="You have rotation" data={data} />
+          <Title>You heavy rotation</Title>
+          <Carousel />
         </ContainerCarousel>
+        <Session>
+          <Title>Music by Mood</Title>
+          <SessionScroll>
+            <TouchableCard>
+              <CardItem>
+                <Card source={fitness} />
+                <Label>Fitness</Label>
+              </CardItem>
+            </TouchableCard>
+            <TouchableCard>
+              <CardItem>
+                <Card source={motivation} />
+                <Label>Motivation</Label>
+              </CardItem>
+            </TouchableCard>
+            <TouchableCard>
+              <CardItem>
+                <Card source={focus} />
+                <Label>Focus</Label>
+              </CardItem>
+            </TouchableCard>
+            <TouchableCard>
+              <CardItem>
+                <Card source={fell} />
+                <Label>Fell</Label>
+              </CardItem>
+            </TouchableCard>
+          </SessionScroll>
+        </Session>
+        <Session>
+          <Title>Don&rsquo;t miss it today</Title>
+          <SessionScroll>
+            <TouchableCard
+              onPress={() =>
+                goToPlaylistScreen(
+                  'Good Vibes only',
+                  good_vibes,
+                  'Playlist by Spotify · 2020'
+                )
+              }
+            >
+              <CardItem>
+                <Card source={good_vibes} />
+                <Label>Good Vibes only</Label>
+                <Description>
+                  The best of yesterday&rsquo;s and today&rsquo;s music
+                </Description>
+              </CardItem>
+            </TouchableCard>
+            <TouchableCard
+              onPress={() =>
+                goToPlaylistScreen(
+                  'Keep the move!',
+                  keep_the_move,
+                  'Playlist by Spotify · 2020'
+                )
+              }
+            >
+              <CardItem>
+                <Card source={keep_the_move} />
+                <Label>Keep the move!</Label>
+                <Description>The supercharged playlist</Description>
+              </CardItem>
+            </TouchableCard>
+            <TouchableCard
+              onPress={() =>
+                goToPlaylistScreen(
+                  'Actual hits',
+                  actual_hits,
+                  'Playlist by Spotify · 2020'
+                )
+              }
+            >
+              <CardItem>
+                <Card source={actual_hits} />
+                <Label>Actual hits</Label>
+                <Description>
+                  Ed Sheran at the topof the first world playlist
+                </Description>
+              </CardItem>
+            </TouchableCard>
+          </SessionScroll>
+        </Session>
       </Container>
     </SafeAreaView>
   );
--- src/screens/home/styled.ts
+++ src/screens/home/styled.ts
@@ -2,14 +2,63 @@ import styled from 'styled-components/native';
 
 export const SafeAreaView = styled.SafeAreaView`
   flex: 1;
-  background-color: ${props => props.theme.colors.backgroundPrimary};
+  background-color: ${props => props.theme.colors.backgroundSecondary};
 `;
 
-export const Container = styled.View`
+export const Container = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+})`
   flex: 1;
-  padding: 0 20px;
 `;
 
 export const ContainerCarousel = styled.View`
-  padding: 20px 0;
+  background-color: ${props => props.theme.colors.backgroundSecondary};
+  padding-bottom: 20px;
+`;
+
+export const Session = styled.View`
+  background-color: ${props => props.theme.colors.backgroundPrimary};
+  padding-bottom: 20px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${props => props.theme.fonts.fontFamilyBold};
+  font-size: ${props => props.theme.fonts.medium};
+  color: ${props => props.theme.colors.primary};
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+  padding: 20px;
+`;
+
+export const SessionScroll = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingLeft: 10, paddingRight: 10 },
+})``;
+
+export const TouchableCard = styled.TouchableOpacity``;
+
+export const CardItem = styled.View`
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const Card = styled.Image`
+  margin-bottom: 5px;
+  border-radius: 11px;
+`;
+
+export const Label = styled.Text`
+  font-family: ${props => props.theme.fonts.fontFamilyMedium};
+  font-size: ${props => props.theme.fonts.small};
+  color: ${props => props.theme.colors.text};
+  letter-spacing: ${props => props.theme.fonts.letterSpacing};
+`;
+
+export const Description = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  width: 122px;
+  font-family: ${props => props.theme.fonts.fontFamilyLight};
+  font-size: ${props => props.theme.fonts.smaller};
+  color: ${props => props.theme.colors.text};
 `;
